2bea6315cbe01e4b5146c033d4610e469a3319bb
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-01-02 13:37:50 +0300

    UE4_HW16_5 done

--- UE4_HW16_5.cpp
+++ UE4_HW16_5.cpp
@@ -3,11 +3,20 @@
 
 int main()
 {
-    const int size = 2;
-    int array[size][size] = { {1,2}, {4,5} };
+    const int size = 10;
+    int array[size][size] = {};
 
-    //std::cout << array[0];
+    // Init array
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            array[i][j] = i+j;
+        }
+    }
 
+    //Printing array into console
+    std::cout << "Your ARRAY:\n";
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
@@ -17,6 +26,15 @@ int main()
         std::cout << std::endl;
     }
 
-     
+    // Set the array line to sum elements
+    int index = 2 % size; 
 
+    //sum elements in the line and print result
+    std::cout << "Sum of the ARRAY line " << index << "\n";
+
+    for (int i = 0, sum = 0; i <= size; i++)
+    {
+        if (i == size) {std::cout << sum << std::endl; break;}
+        sum += array[index][i];
+    }
 }
